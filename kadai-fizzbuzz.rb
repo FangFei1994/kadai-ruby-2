d2821560fe1fbd3242e6759456d26470b8519f50
FangFei1994 <a13061121616@gmail.com>
2022-01-12 08:51:28 +0000

    fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,16 @@
+num_max = 100
+def fizzbuzz(num)
+  if num % 3 == 0 and num % 5 == 0
+      puts "FizzBuzz"
+  elsif num % 5 == 0
+      puts "Buzz"
+  elsif num % 3 == 0 
+      puts "Fizz"
+  else 
+      puts num
+  end
+end
+
+(1..num_max).each do |num|
+    fizzbuzz(num)
+end
